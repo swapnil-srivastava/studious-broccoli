a6039ec807ac4f14237fc55e97ddaaa257527354
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2024-09-15 23:32:59 +0000

    adding the pages section to the app

--- app.vue
+++ app.vue
@@ -1,6 +1,14 @@
+<!-- app.vue -->
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
+
+<script setup>
+
+</script>
+
+<style>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="text-center mt-10">
+    <h1 class="text-2xl font-bold">Hello World</h1>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+
+</style>
