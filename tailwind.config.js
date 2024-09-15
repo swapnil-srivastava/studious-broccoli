b39f1c09967a709f617b3b66a0a0c4fd744d7777
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2024-09-15 21:46:14 +0000

    adding the tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    './pages/**/*.{vue,js}',
+    './components/**/*.{vue,js}',
+    './layouts/**/*.{vue,js}',
+    './plugins/**/*.{js,ts}',
+    './nuxt.config.{js,ts}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
