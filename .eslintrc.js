bcba341b139aee6e6cac2ea4ed5668ae0a05058e
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2024-09-15 21:53:46 +0000

    adding eslintrc

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    extends: [
+      'eslint:recommended',
+      'plugin:vue/vue3-recommended',
+      'prettier'
+    ],
+    rules: {
+      // Add custom rules here
+    }
+  }
