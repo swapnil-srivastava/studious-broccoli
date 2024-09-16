914485671c5b06b35fc1cb216193226be1ad5611
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2024-09-16 02:53:07 +0000

    adding check for repo, fetching the search parameters and showing the result in the index vue page

--- pages/index.vue
+++ pages/index.vue
@@ -5,23 +5,38 @@
     <div v-if="githubStore.loading" class="mt-8 text-center text-gray-600">
       <span class="animate-spin inline-block mr-2">⌛</span> Loading...
     </div>
-    <div v-else-if="githubStore.error">{{ githubStore.error }}</div>
-      <RepositoryList v-else
+    <template v-else>
+      <div v-if="searchSummary" class="mt-6 mb-4 p-4 bg-blue-100 rounded-lg text-blue-800">
+        {{ searchSummary }}
+      </div>
+      <RepositoryList
+        v-if="githubStore.repositories && Object.keys(githubStore.repositories).length > 0"
         :repositories="githubStore.repositories" 
         :languages="githubStore.searchedLanguages"
         @loadMore="handleLoadMore"
       />
+      <div v-else class="mt-8 text-center text-gray-600">
+        No repositories found matching your criteria.
+      </div>
+    </template>
   </div>
 </template>
 
 <script setup lang="ts">
-import { useGithubStore } from '~/stores/github'
+import { useGithubStore } from '~/stores/github';
 import type { SearchParams } from '../types/github';
+const searchSummary = ref('');
 
-const githubStore = useGithubStore()
+const githubStore = useGithubStore();
 
 const handleSearch = (searchParams: SearchParams) => {
-  githubStore.searchRepositories(searchParams)
+  githubStore.searchRepositories(searchParams);
+
+  const languagesText = searchParams.languages.length > 0 
+    ? `for ${searchParams.languages.join(', ')} ` 
+    : ''
+  
+  searchSummary.value = `GitHub repositories ${languagesText}between ${searchParams.startDate} and ${searchParams.endDate} with at least ${searchParams.minStars} stars`;
 }
 
 const handleLoadMore = (language: string) => {
