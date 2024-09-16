d7572696dcc29534e1465b5b5f46735b36d108f0
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2024-09-16 02:16:38 +0000

    moved the types to common place

---
+++ types/github.ts
@@ -0,0 +1,25 @@
+export interface Repository {
+    id: number;
+    name: string;
+    html_url: string;
+    description: string | null;
+    language: string | null;
+    stargazers_count: number;
+    created_at: string;
+  }
+  
+export interface GithubState {
+    repositories: Record<string, Repository[]>;
+    searchedLanguages: string[];
+    loading: boolean;
+    error: string | null;
+    currentPage: Record<string, number | null>;
+    lastSearchParams: SearchParams;
+}
+  
+export interface SearchParams {
+    languages: string[];
+    startDate: string;
+    endDate: string;
+    minStars: number;
+}
